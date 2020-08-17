b1c813f2d84c134ccf7a6aa5f218cdf5c549b3f2
Johnathan Meunier <johnathanmeunier@MacBook-Pro.local>
2020-08-17 13:54:41 +0200

    feat: podcast selector

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Chapter from "./Chapter";
 import NewChapter from "./NewChapter";
 import Player from "./Player";
+import data from "./data.json";
 
 import "./App.css";
 
@@ -32,48 +33,8 @@ const App = () => {
   const [currentSound, setCurrentSound] = useState();
   const [timer, setTimer] = useState(0);
   const [timerInterval, setTimerInterval] = useState();
-  const [chapters, setChapters] = useState([
-    {
-      name: "Intro",
-      startTime: null,
-      stopTime: null,
-      author: "SH",
-      notes: ["Lorem ipsum", "sit dolor", "amet"],
-      current: false,
-    },
-    {
-      name: "Apple vs Epic",
-      startTime: null,
-      stopTime: null,
-      author: "John",
-      notes: ["Lorem ipsum", "sit dolor", "amet"],
-      current: false,
-    },
-    {
-      name: "Tenet",
-      startTime: null,
-      stopTime: null,
-      author: "John",
-      notes: ["Lorem ipsum", "sit dolor", "amet"],
-      current: false,
-    },
-    {
-      name: "Mulan",
-      startTime: null,
-      stopTime: null,
-      author: "John",
-      notes: ["Lorem ipsum", "sit dolor", "amet"],
-      current: false,
-    },
-    {
-      name: "iPhone",
-      startTime: null,
-      stopTime: null,
-      author: "John",
-      notes: ["Lorem ipsum", "sit dolor", "amet"],
-      current: false,
-    },
-  ]);
+  const [podcastId, setPodcastId] = useState(null);
+  const [chapters, setChapters] = useState(podcastId !== null ? data.podcasts[podcastId].chapters : []);
 
   useEffect(() => {
     setChapters(chapters.sort((a, b) => (a.startTime !== null ? a.startTime - b.startTime : 0)));
@@ -83,7 +44,7 @@ const App = () => {
     <div className="App">
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-2">
+          <div className="col-md-2 sounds">
             <h3>Sounds</h3>
             {sounds.map(({ name, duration }) => (
               <div className="row" key={name}>
@@ -105,79 +66,119 @@ const App = () => {
             ))}
             <Player currentSound={currentSound} />
           </div>
-          <div className="col-md-10">
+
+          <div className="col-md-10 podcast">
             <h1>Podcast tool</h1>
-            <h2>Apple vs Epic, Tenet 02x01</h2>
-            <h3>{timer}</h3>
-            {chapters[0].startTime === null && (
-              <button
-                type="button"
-                className="btn btn-success"
-                onClick={() => {
-                  setCurrentSound("jingle");
-                  setChapters((prevChapters) => {
-                    prevChapters[0].startTime = timer;
-                    prevChapters[0].current = true;
-                    return prevChapters;
-                  });
-                  setTimeout(() => {
-                    setCurrentSound("bed");
-                  }, 11000);
-                  const myInt = setInterval(() => {
-                    setTimer((prevTimer) => {
-                      return prevTimer + 1;
-                    });
-                  }, 1000);
-                  setTimerInterval(myInt);
-                }}
-              >
-                Start
-              </button>
-            )}
-            {chapters[0].startTime !== null && (
-              <button
-                type="button"
-                className="btn btn-danger"
-                onClick={() => {
-                  clearInterval(timerInterval);
-                  console.log(chapters);
-                  setChapters(
-                    chapters.map((chapter) => {
-                      chapter.current = false;
-                      if (chapter.startTime === null) {
-                        chapter.startTime = timer;
-                      }
-                      if (chapter.stopTime === null) {
-                        chapter.stopTime = timer;
-                      }
-                      return chapter;
-                    })
-                  );
-                  clearInterval(timerInterval);
-                }}
-              >
-                Stop
-              </button>
-            )}
-            <div className="row row-cols-2">
-              {chapters.map(({ name, notes, startTime, stopTime, author, current }, index) => (
-                <Chapter
-                  key={name}
-                  name={name}
-                  notes={notes}
-                  startTime={startTime}
-                  stopTime={stopTime}
-                  author={author}
-                  index={index}
-                  setCurrentSound={setCurrentSound}
-                  setChapters={setChapters}
-                  timer={timer}
-                  current={current}
-                  podcastStarted={chapters[0].startTime !== null}
-                />
+            <h2>Select podcast</h2>
+            <select
+              className="form-group form-control"
+              value={podcastId}
+              onChange={(e) => {
+                setPodcastId(e.target.value);
+                setChapters(data.podcasts[e.target.value].chapters);
+              }}
+            >
+              <option value={null}>Select a podcast</option>
+              {data.podcasts.map(({ title, season, episode }, index) => (
+                <option value={index}>
+                  {title} {season}x{episode}
+                </option>
               ))}
-              <NewChapter setChapters={setChapters} />
-            </div>
+            </select>
+            {podcastId !== null && (
+              <>
+                <h2>
+                  {data.podcasts[podcastId].title} {data.podcasts[podcastId].season}x{data.podcasts[podcastId].episode}
+                </h2>
+                <h3>{timer}</h3>
+                {chapters[0].startTime === null && (
+                  <button
+                    type="button"
+                    className="btn btn-success"
+                    onClick={() => {
+                      setCurrentSound("jingle");
+                      setChapters((prevChapters) => {
+                        prevChapters[0].startTime = timer;
+                        prevChapters[0].current = true;
+                        return prevChapters;
+                      });
+                      setTimeout(() => {
+                        setCurrentSound("bed");
+                      }, 11000);
+                      const myInt = setInterval(() => {
+                        setTimer((prevTimer) => {
+                          return prevTimer + 1;
+                        });
+                      }, 1000);
+                      setTimerInterval(myInt);
+                    }}
+                  >
+                    Start
+                  </button>
+                )}
+                {chapters[0].startTime !== null && (
+                  <button
+                    type="button"
+                    className="btn btn-danger"
+                    onClick={() => {
+                      clearInterval(timerInterval);
+                      console.log(chapters);
+                      setChapters(
+                        chapters.map((chapter) => {
+                          chapter.current = false;
+                          if (chapter.startTime === null) {
+                            chapter.startTime = timer;
+                          }
+                          if (chapter.stopTime === null) {
+                            chapter.stopTime = timer;
+                          }
+                          return chapter;
+                        })
+                      );
+                      clearInterval(timerInterval);
+                    }}
+                  >
+                    Stop
+                  </button>
+                )}
+                <div className="row row-cols-2">
+                  {chapters.map(({ name, notes, startTime, stopTime, author, current }, index) => (
+                    <Chapter
+                      key={name}
+                      name={name}
+                      notes={notes}
+                      startTime={startTime}
+                      stopTime={stopTime}
+                      author={author}
+                      index={index}
+                      setCurrentSound={setCurrentSound}
+                      setChapters={setChapters}
+                      timer={timer}
+                      current={current}
+                      podcastStarted={chapters[0].startTime !== null}
+                    />
+                  ))}
+                  <NewChapter setChapters={setChapters} />
+                </div>
+                <div className="export">
+                  <h3>Summary</h3>
+                  <div className="export-summary">
+                    <h4>Titre</h4>
+                    {data.podcasts[podcastId].title} {data.podcasts[podcastId].season}x{data.podcasts[podcastId].episode}
+                    <h4>Description</h4>
+                    Hello ! Début de la description
+                    {data.podcasts[podcastId].chapters.map(({ name, startTime }) => (
+                      <p>
+                        {startTime} - {name}
+                      </p>
+                    ))}
+                    Fin de la description
+                  </div>
+                  <h3>Timecode</h3>
+                  <div className="export-timecode">Format pour l'export audacity / ffmpeg ?</div>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
