52785ae36911a87a31af8690217ce9e86f6539f9
Johnathan Meunier <johnathanmeunier@MacBook-Pro.local>
2020-08-17 14:55:59 +0200

    feat: padStart time

--- src/services/date.js
+++ src/services/date.js
@@ -1,1 +1,1 @@
-export const secondsToMMSS = (seconds) => `${Math.floor(seconds / 60)}:${seconds % 60}`;
+export const secondsToMMSS = (seconds) => `${(Math.floor(seconds / 60) + "").padStart(2, "0")}:${((seconds % 60) + "").padStart(2, "0")}`;
