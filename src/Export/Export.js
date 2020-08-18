19dd59bd709da2bad46558ab32775f5ca1b7b496
Johnathan Meunier <johnathanmeunier@MacBook-Pro.local>
2020-08-18 11:13:37 +0200

    feat(Export): improve export description

--- src/Export/Export.js
+++ src/Export/Export.js
@@ -1,27 +1,84 @@
-import React from "react";
+import React, { useRef } from "react";
+
+import { secondsToMMSS } from "../services/date";
 
 const Export = ({ data, podcastId }) => {
+  const $exportTimecode = useRef();
   return (
-    <div className="export" contentEditable>
+    <div className="export">
       <h3>Summary</h3>
-      <div className="export-summary">
+      <div className="export-summary" contentEditable>
         <h4>Titre</h4>
         {data.podcasts[podcastId].title} {data.podcasts[podcastId].season}x{data.podcasts[podcastId].episode}
         <h4>Description</h4>
         Hello ! Début de la description
         {data.podcasts[podcastId].chapters.map(({ name, startTime }) => (
           <p>
-            {startTime} - {name}
+            {secondsToMMSS(startTime)} - {name}
           </p>
         ))}
-        Vous pouvez une nouvelle fois nous écouter sur Anchor : https://anchor.fm/techcast-podcast ! L’enregistrement s’est fait via discord, donc si vous
-        souhaitez participer n’hésitez pas : http://discord.techcast.fr N'hésitez pas à nous faire vos retours grâce au hashtag #TechCast et nous retrouver sur
-        twitter : Johnathan MEUNIER : @J7N__ Charles-Henri DUMALIN : @CallMeSH. Victor CHARTIER : @Chabarnak Merci également pour tous les partages et les
-        abonnements. Matériels : Micro & pied : https://amzn.to/2Lweire Table de mixage : https://amzn.to/2Al0idg Enregistreur : https://amzn.to/2Lzoori iPad :
-        https://amzn.to/2CTjyzT Bird UM1 : https://amzn.to/2vVAgyV Code Shadow : John : JOHHJWQB SH : CHANEOJH
+        <p>
+          Vous pouvez retrouver tous nos podcasts et les différents plateformes sur Anchor :{" "}
+          <a href="https://anchor.fm/techcast-podcast">https://anchor.fm/techcast-podcast</a> !{" "}
+        </p>
+        <p>L’enregistrement se fait via discord, donc si vous souhaitez participer n’hésitez pas : http://discord.techcast.fr </p>
+        <p>
+          N'hésitez pas non plus à nous faire vos retours grâce au hashtag #CPLS et nous retrouver sur twitter :
+          <ul>
+            <li>
+              Johnathan MEUNIER : <a href="https://twitter.com/J7N__">@J7N__</a>
+            </li>
+            <li>
+              Charles-Henri DUMALIN : <a href="https://twitter.com/CallMeSH">@CallMeSH</a>
+            </li>
+          </ul>
+          <p>Merci également pour tous les partages et les abonnements. </p>
+          <p>Matériels : </p>
+          <ul>
+            <li>
+              Micro & pied : <a href="https://amzn.to/2Lweire">https://amzn.to/2Lweire</a>
+            </li>
+            <li>
+              Table de mixage : <a href="https://amzn.to/2Al0idg">https://amzn.to/2Al0idg</a>
+            </li>
+            <li>
+              Enregistreur : <a href="https://amzn.to/2Lzoori">https://amzn.to/2Lzoori</a>
+            </li>
+            <li>
+              iPad : <a href="https://amzn.to/2CTjyzT">https://amzn.to/2CTjyzT</a>
+            </li>
+            <li>
+              Bird UM1 : <a href="https://amzn.to/2vVAgyV">https://amzn.to/2vVAgyV</a>
+            </li>
+          </ul>
+          <p>Code Shadow :</p>
+          <ul>
+            <li>John : JOHHJWQB</li>
+            <li>SH : CHANEOJH</li>
+          </ul>
+        </p>
       </div>
       <h3>Timecode</h3>
-      <div className="export-timecode">Format pour l'export audacity / ffmpeg ?</div>
+      <button
+        className="btn btn-info"
+        onClick={() => {
+          const el = document.createElement("textarea");
+          el.value = $exportTimecode.current.innerText;
+          document.body.appendChild(el);
+          el.select();
+          document.execCommand("copy");
+          document.body.removeChild(el);
+        }}
+      >
+        Copy Timecode
+      </button>
+      <pre className="export-timecode" ref={$exportTimecode}>
+        {data.podcasts[podcastId].chapters.map(({ name, startTime, stopTime }, i) => (
+          <p>
+            {startTime}.000000&#9;{stopTime}.000000&#9;{(i + 1 + "").padStart(2, "0")} - {name}
+          </p>
+        ))}
+      </pre>
     </div>
   );
 };
