526db3e93401de19fc4a6bd5bb722d4a514c0581
Johnathan Meunier <johnathanmeunier@MacBook-Pro.local>
2020-08-27 13:57:14 +0200

    feat(Export): titre coupé pour la génération de vignette

--- src/Export/Export.js
+++ src/Export/Export.js
@@ -2,21 +2,42 @@ import React, { useRef, useEffect } from "react";
 
 import { secondsToMMSS } from "../services/date";
 
+import logo from "./vignette.jpg";
+
+const transformTitle = (title) => {
+  let array = [];
+  let tmp = [];
+  const titleArray = title.split(" ");
+  for (let i = 0; i < titleArray.length; i++) {
+    tmp.push(titleArray[i]);
+    if (tmp.join(" ").length >= 15) {
+      array.push(tmp.join(" "));
+      tmp = [];
+    } else {
+    }
+  }
+  return array;
+};
+
 const Export = ({ data, podcastId }) => {
   const $exportTimecode = useRef();
   const $thumbnail = useRef();
-  const title = `${data.podcasts[podcastId].title} ${data.podcasts[podcastId].season}x${data.podcasts[podcastId].episode}`;
+  const title = `${data.podcasts[podcastId].title} ${data.podcasts[podcastId].season.toString().padStart(2, "0")}x${data.podcasts[podcastId].episode
+    .toString()
+    .padStart(2, "0")}`;
   useEffect(() => {
-    console.log($thumbnail);
     const canvas = $thumbnail.current;
     const ctx = canvas.getContext("2d");
     var background = new Image();
-    background.src =
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.techcastglobal.com%2Fwp-content%2Fuploads%2F2016%2F04%2FTechCast_Logo_Vertical_Slogan-300x252.jpg&f=1&nofb=1";
+    background.src = logo;
     background.onload = function () {
-      ctx.drawImage(background, 0, 0);
-      ctx.font = "30px Arial";
-      ctx.fillText(title, 10, 50);
+      ctx.drawImage(background, 0, 0, 1000, 1000);
+      ctx.font = "90px KeepCalm";
+      ctx.textAlign = "center";
+      console.log(title.length, transformTitle(title));
+      transformTitle(title).forEach((text, i) => {
+        ctx.fillText(text, 500, 700 + i * 100);
+      });
     };
   }, [data, title]);
 
@@ -37,8 +58,7 @@ const Export = ({ data, podcastId }) => {
             </p>
           ))}
         <p>
-          Vous pouvez retrouver tous nos podcasts et les différents plateformes sur Anchor :{" "}
-          <a href="https://anchor.fm/techcast-podcast">https://anchor.fm/techcast-podcast</a> !{" "}
+          Vous pouvez retrouver tous nos podcasts et les différents plateformes sur Anchor : <a href="https://anchor.fm/cpls">https://anchor.fm/cpls</a> !{" "}
         </p>
         <p>L’enregistrement se fait via discord, donc si vous souhaitez participer n’hésitez pas : http://discord.techcast.fr </p>
         <p>N'hésitez pas non plus à nous faire vos retours grâce au hashtag #CPLS et nous retrouver sur twitter :</p>
@@ -76,7 +96,7 @@ const Export = ({ data, podcastId }) => {
         </ul>
       </div>
       <h3>Vignette</h3>
-      <canvas width="600" height="600" ref={$thumbnail}></canvas>
+      <canvas width="1000" height="1000" ref={$thumbnail}></canvas>
       <h3>
         Timecode{" "}
         <button
